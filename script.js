45a581030a01cf3fe4a340d1dbc62a4e17c0ce04
Jasper Nielsen <jasper.nielsen@hotmail.com>
2023-04-14 14:33:01 +0200

    made the app work

--- script.js
+++ script.js
@@ -4,41 +4,30 @@ window.addEventListener("load", initApp);
 
 const array = [];
 
-let queueSize;
-
-
 function initApp() {
-//   setInterval(putCustomerInArray, 500);
- setInterval(readArraySetBarHeight, 2000);
-// readArraySetBarHeight();
-
+  //   setInterval(putCustomerInArray, 500);
+  setInterval(readArraySetBarHeight, 500);
 }
 
 //gives us a random number between 1 and 32
-function getNumberOfCUstormers() {
+function getNumberOfCustormers() {
   return Math.floor(Math.random() * 32);
 }
 
 function putCustomerInArray() {
-  queueSize = getNumberOfCUstormers();
+  const queueSize = getNumberOfCustormers();
   array.push(queueSize);
   if (array.length > 40) {
     array.shift();
   }
-  
-// return array;
-//   console.log(array);
 }
 
 function readArraySetBarHeight() {
-
-
-    for (let i = 1; i < 41; i++) {
-      const myHeight = queueSize * 40;
-      document.querySelector(`#col${i}`).style.height = `${myHeight}%`;
-      putCustomerInArray(); 
-      console.log(`#col${i}`);
-      console.log(array);
-    }
+  //placer den uden for loopet
+  putCustomerInArray();
+  for (let i = 1; i < 41; i++) {
+    document.querySelector(`#col${i}`).style.height = `${array[i - 1] * 100}%`;
+    //i-1 for at få fat i første index i arrayet og matche det til col1.
   }
-
+  console.log(array);
+}
